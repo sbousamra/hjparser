0ef5e2d90cf832c65846f070836f7163bf708a06
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2019-08-29 23:27:15 +1000

    feat(parser): added JBool, JArray, Jnumber

--- src/Parser.hs
+++ src/Parser.hs
@@ -13,17 +13,18 @@ data JSON = JString String
 parse :: String -> Maybe JSON
 parse v = Just (JString v)
 
--- { "cock": "smith", 
---   "grin": "meat" 
--- }
-
 dummy :: JSON
-dummy = JObject [("cock", JString "smith"), ("grin", JString "meat")]
+dummy = JObject [("dummy", JString "json"), ("key", JString "value")]
 
 renderKeyValue :: (String, JSON) -> String
 renderKeyValue (k, v) = "\"" ++ k ++ "\"" ++ ": " ++ render v
 
 render :: JSON -> String
 render v = case v of
-  JString i -> "\""  ++ i ++ "\""
+  JString i -> "\"" ++ i ++ "\""
+  JNumber i -> show i
+  JBool True -> "true" 
+  JBool False -> "false"
+  JNull -> "null"
   JObject i -> "{ " ++ (intercalate ",\n" $ fmap renderKeyValue i) ++ " }"
+  JArray i -> "[" ++ (intercalate ",\n" $ fmap render i) ++ "]"
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,14 @@ tests :: TestTree
 tests = testGroup "Tests" [renderTests]
 
 renderTests :: TestTree
-renderTests = testGroup "Render Tests" [renderStringTest, renderObjectTest]
+renderTests = testGroup "Render Tests" 
+  [ renderStringTest
+  , renderNumberTest
+  , renderBoolTest
+  , renderNullTest
+  , renderObjectTest
+  , renderArrayTest
+  ]
 
 renderStringTest :: TestTree
 renderStringTest = testCase "Render a string" $ do
@@ -17,11 +24,42 @@ renderStringTest = testCase "Render a string" $ do
   let expected = "\"Test string\""
   let actual = render subject
   actual @?= expected
+
+renderNumberTest :: TestTree
+renderNumberTest = testCase "Render a number" $ do
+  let subject = JNumber 1.0
+  let expected = "1.0"
+  let actual = render subject
+  actual @?= expected
+  
+renderBoolTest :: TestTree
+renderBoolTest = testCase "Render a bool" $ do
+  let subject = JBool True
+  let expected = "true"
+  let actual = render subject
+  actual @?= expected
+
+renderNullTest :: TestTree
+renderNullTest = testCase "Render a null" $ do
+  let subject = JNull
+  let expected = "null"
+  let actual = render subject
+  actual @?= expected
   
 renderObjectTest :: TestTree
 renderObjectTest = testCase "Render a object" $ do
   let subject = JObject [("test", JString "object"), ("key", JString "value")]
   let expected = "{ \"test\": \"object\",\n\"key\": \"value\" }"
+  let actual = render subject
+  actual @?= expected
+
+renderArrayTest :: TestTree
+renderArrayTest = testCase "Render a array" $ do
+  let subject = 
+        JArray [
+          JObject [("test", JString "array"), ("key", JString "value")]
+        ]
+  let expected = "[{ \"test\": \"array\",\n\"key\": \"value\" }]"
   let actual = render subject
   actual @?= expected
 
